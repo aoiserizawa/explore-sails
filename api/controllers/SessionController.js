837d7b647e24460530061586748bced66a28c051
alvin <alvinjosephvaldez@yahoo.com>
2014-08-18 05:43:43 +0800

    added a password error

--- api/controllers/SessionController.js
+++ api/controllers/SessionController.js
@@ -18,14 +18,50 @@
 module.exports = {
     
     'new': function(req, res){
-
-        req.session.authenticated = true;
-        console.log(req.session);
-
         res.view('session/new');
-    }  
+    }, 
 
+    'create': function (req, res, next){
 
+        if(!req.param('email') && !req.param('password')){
+            var usernamePasswordRequiredError = [{name: 'usernamePasswordRequired', message: 'You must enter both a username and password. '}];
+           
+            req.session.flash = {
+                err: usernamePasswordRequiredError
+            };
+            res.redirect('/session/new');
+            return;
+        }
 
+        User.findOneByEmail(req.param('email'), function foundUser(err, user){
+            if(err) return next(err);
+
+            if(!user){
+                var noAccountError = [{name:'noAccount', message: 'The email address '+ req.param('email')+' not found.'}];
+                req.session.flash = {
+                    err: noAccountError
+                };
+                res.redirect('/session/new');
+                return;
+            }
+
+            bcrypt.compare(req.param('password'), user.encryptedPassword, function(err, valid){
+                if(err) return next(err);
+
+                // If the password from the form doesn't match the password from the database...
+                if (!valid) {
+                    var usernamePasswordMismatchError = [{
+                        name: 'usernamePasswordMismatch',
+                        message: 'Invalid username and password combination.'
+                    }]
+                    req.session.flash = {
+                        err: usernamePasswordMismatchError
+                    }
+                    res.redirect('/session/new');
+                    return;
+                }
+            });
+        });
+    }
   
 };
